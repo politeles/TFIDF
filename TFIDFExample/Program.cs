16e6dccca19338f8f9db515a0669f6745329ce10
José Enrique <politeles@users.noreply.github.com>
2017-06-20 16:15:39 +0200

    Using TF-IDF with XGBoost example

--- TFIDFExample/Program.cs
+++ TFIDFExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XGBoost;
 
 namespace TFIDFExample
 {
@@ -10,29 +11,40 @@ namespace TFIDFExample
     {
         static void Main(string[] args)
         {
-            // Some example documents.
-            string[] documents =
+            // Some example email body.
+            string[] email =
             {
-                "The sun in the sky is bright.",
-                "We can see the shining sun, the bright sun."
+                "I have a problem with my payroll",
+                "I'm gonna leave this company.",
+                "I'm going on holidays",
+                "I can't see my payroll",
             };
 
+            // let's say this is the classification for each email:
+            float[] category = { 0, 1, 1, 0 };
+            // category 0 => payroll
+            // category 1 => other stuff
+
+
             // Apply TF*IDF to the documents and get the resulting vectors.
-            double[][] inputs = TFIDF.Transform(documents, 0);
+            // This is what I call train on TFIDF:
+            double[][] inputs = TFIDF.Transform(email, 0);
             inputs = TFIDF.Normalize(inputs);
 
-            // Display the output.
-            for (int index = 0; index < inputs.Length; index++)
-            {
-                Console.WriteLine(documents[index]);
+            // Training for XGBoost (the classifier):
+            var classifier = new XGBClassifier();
+            classifier.Fit(inputs, category);
 
-                foreach (double value in inputs[index])
-                {
-                    Console.Write(value + ", ");
-                }
 
-                Console.WriteLine("\n");
-            }
+            // Once is trained, we want to classify a new text:
+            string[] new_email =
+                { "Hey, I didn't get any money this month, can you check what's going on with my payroll?"};
+            // then, we have to apply the TFIDF, based on the documents we already have
+            double[][] new_inputs = TFIDF.Transform(new_email, 0);
+            new_inputs = TFIDF.Normalize(new_inputs);
+
+            // predict!
+            float predicted_class = classifier.Predict(new_inputs);
 
             Console.WriteLine("Press any key ..");
             Console.ReadKey();
